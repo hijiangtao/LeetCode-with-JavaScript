9d4b63458f2fbab984429cd34e771773b42c751e
Joe Jiang <hijiangtao@gmail.com>
2017-04-13 23:33:45 +0800

    update

--- src/arithmetic-slices/res.js
+++ src/arithmetic-slices/res.js
@@ -52,3 +52,19 @@ let Sum = function(n) {
 	// console.log(n);
 	return (n - 2) * (n - 1) / 2;
 };
+
+/**
+ * Another solution
+ */
+// let solution = function(A) {
+// 	let curr = 0,
+// 		sum = 0;
+// 	for (let i = 2; i < A.length; i++)
+// 		if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+// 			curr += 1;
+// 			sum += curr;
+// 		} else {
+// 			curr = 0;
+// 		}
+// 	return sum;
+// }
