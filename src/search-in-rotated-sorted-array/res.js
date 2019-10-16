0d214bbb4c142eac2893c262a07c9cf1c04487a9
hijiangtao <hijiangtao@gmail.com>
2019-10-16 23:25:56 +0800

    update: 33

--- src/search-in-rotated-sorted-array/res.js
+++ src/search-in-rotated-sorted-array/res.js
@@ -54,3 +54,21 @@ let search = function(nums, target) {
     
     return -1;
 };
+
+const search_2 = (nums, target) => {
+    let lo = 0, hi = nums.length - 1;
+
+    while (lo < hi) {
+        let mid = (lo + hi) >> 1;
+
+        if (nums[0] <= nums[mid] && (target > nums[mid] || target < nums[0])) {
+            lo = mid + 1;
+        } else if (target < nums[0] && target > nums[mid]) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+
+    return lo == hi && nums[lo] == target ? lo : -1;
+}
