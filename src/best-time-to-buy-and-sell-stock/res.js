362a13088c375127efa1f071fb0c471e6a3fb0e5
hijiangtao <hijiangtao@gmail.com>
2019-10-20 01:13:31 +0800

    update: DP method

--- src/best-time-to-buy-and-sell-stock/res.js
+++ src/best-time-to-buy-and-sell-stock/res.js
@@ -23,3 +23,20 @@ var maxProfit = function(prices) {
 
   return res;
 };
+
+/**
+ * DP 解法
+ * @param {*} prices 
+ */
+var maxProfit_2 = (prices) => {
+  if (prices.length < 2) return 0;
+
+  let last = 0, max = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    last = Math.max(0, last + prices[i] - prices[i-1]);
+    max = Math.max(last, max);
+  }
+
+  return max;
+}
