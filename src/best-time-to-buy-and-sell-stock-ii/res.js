aa5b8b8015aa460ae3e10366e69bd454b1ee4093
hijiangtao <hijiangtao@gmail.com>
2019-10-20 20:39:21 +0800

    implement: 122

--- src/best-time-to-buy-and-sell-stock-ii/res.js
+++ src/best-time-to-buy-and-sell-stock-ii/res.js
@@ -27,3 +27,48 @@ var maxProfit = function(prices) {
   
   return res;
 };
+
+/**
+ * 一次遍历
+ * @param {*} prices 
+ */
+var maxProfit = function(prices) {
+  const len = prices.length;
+  if (len < 2) return 0;
+  let res = 0;
+
+  for (let i = 1; i < len; i++) {
+    const temp = prices[i] - prices[i-1];
+    if (temp > 0) {
+      res += temp;
+    }
+  }
+
+  return res;
+}
+
+/**
+ * 找到每次单调进行处理
+ * @param {*} prices 
+ */
+var maxProfit = function(prices) {
+  const len = prices.length;
+  if (len < 2) return 0;
+  let res = 0;
+  let peak = prices[0], valley = prices[0];
+  
+  let i = 1;
+  while (i < len) {
+    while (i < len && prices[i] - prices[i-1] <= 0) {
+      i++;
+    }
+    valley = prices[i-1];
+    while (i < len && prices[i] - prices[i-1] >= 0) {
+      i++;
+    }
+    peak = prices[i-1];
+    res += peak - valley;
+  }
+
+  return res;
+}
