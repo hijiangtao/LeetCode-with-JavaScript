b2a48d75b5934b5e736728e6702410106a7b2ac8
hijiangtao <hijiangtao@gmail.com>
2022-06-23 17:59:27 +0800

    update: 111

---
+++ src/minimum-depth-of-binary-tree/res.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function minDepth(root: TreeNode | null): number {
+    if (!root) {
+        return 0;
+    }
+    
+    if (!root.left && !root.right) {
+        return 1;
+    }
+
+    if (root.left && root.right) {
+        return Math.min(minDepth(root.left), minDepth((root.right))) + 1;
+    } else {
+        return Math.max(minDepth(root.left), minDepth((root.right))) + 1;
+    }
+};
