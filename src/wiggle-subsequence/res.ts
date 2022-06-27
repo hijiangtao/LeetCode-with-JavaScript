6dd1267edad1c2c4142daacc48888aca0a93f813
hijiangtao <hijiangtao@gmail.com>
2022-06-27 20:03:57 +0800

    update: 376-2

--- src/wiggle-subsequence/res.ts
+++ src/wiggle-subsequence/res.ts
@@ -11,3 +11,24 @@ function wiggleMaxLength(nums: number[]): number {
 
     return Math.max(up, down);
 };
+
+function wiggleMaxLength2(nums: number[]): number {
+    if (nums.length < 2) {
+        return nums.length;
+    }
+
+    let currentDiff = 0;
+    let previousDiff = 0;
+    let result = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        currentDiff = nums[i] - nums[i-1];
+
+        if (currentDiff>0 && previousDiff<=0 || currentDiff<0 && previousDiff>=0) {
+            previousDiff = currentDiff;
+            result++;
+        }
+    }
+
+    return result+1;
+}
