44d90ce4fd5aad0d7bdb2fd2cc33d65a01eba4ed
Ali Bektash <agokselb@gmail.com>
2022-08-09 14:39:38 +0900

    Typescript version
    Typescript version of #1-two-sum

---
+++ src/two-sum/res.ts
@@ -0,0 +1,29 @@
+/**
+ * Problem: Given an array of integers, return indices of the two numbers such that they add up to a specific target. 
+ * 
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * two-sum.js
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ const twoSum =(nums:Array<number>, target:number): Array<number>  => {
+
+	for (let i = nums.length - 1; i >= 0; i--) {
+		for (let j = 0; j < i; j++) {
+			if ( addition(nums[i], nums[j]) === target ) {
+				return [j, i];
+			}
+		}
+	}
+
+    return [];
+};
+
+// add a with b and return it
+const addition = (a:number, b:number) => {
+	return a + b;
+};
